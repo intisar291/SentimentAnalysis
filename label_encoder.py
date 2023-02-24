91071f0adce4cfb0c40600431061e1dc7e4af5f2
Asir <83521671+intisar291@users.noreply.github.com>
2023-02-24 20:17:09 +0100

    Update label_encoder.py

--- label_encoder.py
+++ label_encoder.py
@@ -2,3 +2,4 @@ def label_encoder():
     encoder=LabelEncoder()
     df["new_label"]=df["label"].apply(lambda x: 1 if x=="positive" else 0) 
     df["new_category"]=df["Category"].apply(lambda x: 1 if x=="collaborative" else 0 )
+    print(self.df.head(5))
